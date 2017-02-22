d1d00f3a96dcc36ae439451c3c35f8434e0c1bfd
rcucchiara <robert_cucchiara@student.uml.edu>
2017-02-21 19:53:02 -0500

    Add files via upload

---
+++ movement/BehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class BehaviourScript : MonoBehaviour
+{
+
+
+    public float playerSpeed = 4f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+    }
+}
